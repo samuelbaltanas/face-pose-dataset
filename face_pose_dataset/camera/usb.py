9a9e5c969cb70f2ca961bad107e3e2bd7cd22a41
Samuel Baltanas <sambalmol@uma.es>
2020-04-22 18:46:02 +0200

    Misc. fixes

--- face_pose_dataset/camera/usb.py
+++ face_pose_dataset/camera/usb.py
@@ -19,11 +19,14 @@ class VideoCamera(object):
         self.video = None
 
     def read_rgb(self):
-        success, image = self.video.read()
+        success, frame = self.video.read()
         while not success:
-            success, image = self.video.read()
+            success, frame = self.video.read()
 
-        return image
+        # DONE: RGB/BGR discrepancy between Astra and opencv cameras
+        frame = cv2.cvtColor(frame.astype("uint8"), cv2.COLOR_BGR2RGB)
+
+        return frame
 
     def read_both(self):
         return self.read_rgb(), None
